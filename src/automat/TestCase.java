b940fd7896071ff6257333e60625baa13cbe71ea
tttt <tttt@tttt-PC>
2017-08-28 01:08:03 -0300

    d

--- src/automat/TestCase.java
+++ src/automat/TestCase.java
@@ -23,13 +23,6 @@ import automat.screens.CustomerScreen;
 import automat.screens.actions.*;
 import automat.screens.data.CustomerScreenData;
 
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author tttt
